3e40f862a432bb210ccae231288245715bb612ae
Romain Quellec <romain.quellec@gmail.com>
2019-02-19 16:25:23 +0100

    add aws profile env

---
+++ loadenv.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const awsParamEnv = require('aws-param-env'); // eslint-disable-line
+
+awsParamEnv.load('/cuistot', { region: 'eu-west-1' });
+
+const content = `\
+AWS_REGION_PARIS=eu-west-3
+AWS_STATIC_DOMAIN=${process.env['domain']}
+`;
+
+fs.writeFile('.env', content, (err) => {
+  if (err) throw err;
+  console.log('.env saved...');
+});
