853ce6a52239954a417c57b9a2488b9afc8e50ab
Romain Quellec <romain.quellec@gmail.com>
2019-02-19 18:04:14 +0100

    test

--- loadenv.js
+++ loadenv.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const awsParamEnv = require('aws-param-env'); // eslint-disable-line
 
 awsParamEnv.load('/cuistot', { region: 'eu-west-1' });
+console.log(process.env);
 
 const content = `\
 AWS_REGION_PARIS=eu-west-3
-AWS_STATIC_DOMAIN=static.cuistotducoin.com
+AWS_STATIC_DOMAIN=${process.env['static/domain']}
 `;
 
 fs.writeFile('.env', content, (err) => {
