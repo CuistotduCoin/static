c202dbf9187f16553452afe93c3fa19945867ec1
Romain Quellec <romain.quellec@gmail.com>
2020-03-19 16:33:37 +0100

    imagemin-pngquant quality change

--- imagemin.js
+++ imagemin.js
@@ -1,12 +1,12 @@
-const imagemin = require('image-2-min');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+const imagemin = require("image-2-min");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
 
 (async () => {
-	await imagemin(['img/**/*.{jpg,png}'], 'static', {
-		plugins: [
-			imageminMozjpeg({ quality: 70 }),
-			imageminPngquant({ quality: '65-80' })
-		]
-	});
+  await imagemin(["img/**/*.{jpg,png}"], "static", {
+    plugins: [
+      imageminMozjpeg({ quality: 70 }),
+      imageminPngquant({ quality: [0.65, 0.8] })
+    ]
+  });
 })();
